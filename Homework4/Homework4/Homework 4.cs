ed0c5fde33f7f52c0224bd1a646883e4e7b50780
Nino_Mtchedlidze <nino.mtchedlidze@iset.ge>
2024-10-21 23:57:43 +0400

    Uploaded Homework 4

--- Homework4/Homework4/Homework 4.cs
+++ Homework4/Homework4/Homework 4.cs
@@ -2,27 +2,27 @@
 
 class Program
 {
-    static char[,] board = new char[3, 3]; 
+    static char[,] board = new char[3, 3];
     static char currentPlayer = 'X';
-    static int row, col; 
+    static int row, col;
     static int movesCount = 0;
 
     static void Main()
     {
         CreateBoard();
 
-        while (true) 
+        while (true)
         {
-            Console.Clear(); 
-            DrawBoard(); 
+            Console.Clear();
+            DrawBoard();
             PlayerInput();
-            movesCount++;   
+            movesCount++;
             if (CheckWinner(board, currentPlayer))
             {
                 Console.Clear();
                 DrawBoard();
-                Console.WriteLine($"Player {currentPlayer} wins!"); 
-                break; 
+                Console.WriteLine($"Player {currentPlayer} wins!");
+                break;
             }
 
             if (movesCount == 9)
@@ -30,7 +30,7 @@ class Program
                 Console.Clear();
                 DrawBoard();
                 Console.WriteLine("It's a draw!");
-                break; 
+                break;
             }
 
             // Switch player/ chats vkitxe :)
@@ -51,14 +51,14 @@ class Program
 
     static void DrawBoard()
     {
-        Console.WriteLine("   0   1   2"); 
+        Console.WriteLine("   0   1   2");
         Console.WriteLine("  -----------");
         for (int i = 0; i < 3; i++)
         {
-            Console.Write(i + " | "); 
+            Console.Write(i + " | ");
             for (int j = 0; j < 3; j++)
             {
-                Console.Write(board[i, j] + " | "); 
+                Console.Write(board[i, j] + " | ");
             }
             Console.WriteLine();
             Console.WriteLine("  -----------");
@@ -81,8 +81,8 @@ class Program
 
                 if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row, col] == ' ')
                 {
-                    board[row, col] = currentPlayer; 
-                    isValidInput = true; 
+                    board[row, col] = currentPlayer;
+                    isValidInput = true;
                 }
                 else
                 {
@@ -99,18 +99,19 @@ class Program
     {
         for (int i = 0; i < 3; i++)
         {
-            if ((board[i, 0] == player && board[i, 1] == player && board[i, 2] == player) || 
-                (board[0, i] == player && board[1, i] == player && board[2, i] == player))   
-            {
+            if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player)
                 return true;
-            }
+
+            if (board[0, i] == player && board[1, i] == player && board[2, i] == player)
+                return true;
         }
-        if ((board[0, 0] == player && board[1, 1] == player && board[2, 2] == player) ||   
-            (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player))   
-        {
+
+        if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
             return true;
-        }
+
+        if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
+            return true;
+
         return false;
     }
-
 }
